e2b76cbeb007a40e73c036f680cdb448c571b321
f0e130 <f0e130@mail.com>
2023-04-30 00:42:02 +0900

    mod tap flavor

---
+++ custom/custom.h
@@ -0,0 +1,13 @@
+#define DEFAULT 0
+#define SYMBOLS 1
+#define NUM     2
+#define NAV     3
+#define CONFIG  4
+
+#define Undo   LC(Z)
+#define Cut    LC(X)
+#define Copy   LC(C)
+#define Paste  LC(V)
+#define Redo   LC(Y)
+
+#define TAPPING_TERM 150
