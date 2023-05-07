a0784cfff66062084cf623c132a948b6d73aa43a
f0e130 <f0e130@mail.com>
2023-05-07 10:25:40 +0900

    add key defines

---
+++ custom/keys.h
@@ -0,0 +1,126 @@
+// keys
+#define KP_Q__ &kp Q
+#define KP_W__ &kp W
+#define KP_E__ &kp E
+#define KP_R__ &kp R
+#define KP_T__ &kp T
+#define KP_Y__ &kp Y
+#define KP_U__ &kp U
+#define KP_I__ &kp I
+#define KP_O__ &kp O
+#define KP_P__ &kp P
+#define KP_S__ &kp S
+#define KP_D__ &kp D
+#define KP_F__ &kp F
+#define KP_G__ &kp G
+#define KP_H__ &kp H
+#define KP_J__ &kp J
+#define KP_K__ &kp K
+#define KP_L__ &kp L
+#define KP_V__ &kp V
+#define KP_B__ &kp B
+#define KP_N__ &kp N
+#define KP_M__ &kp M
+
+#define KP_ESC &kp ESCAPE
+#define KP_TAB &kp TAB
+#define KP_SPC &kp SPACE
+#define KP_BSP &kp BACKSPACE
+#define KP_EXC &kp EXCL
+#define KP_AT_ &kp AT
+#define KP_HSH &kp HASH
+#define KP_DLR &kp DLLR
+#define KP_PCT &kp PRCNT
+#define KP_CRT &kp CARET
+#define KP_AMP &kp AMPS
+#define KP_AST &kp ASTRK
+#define KP_UND &kp UNDER
+#define KP_EQL &kp EQUAL
+#define KP_GRV &kp GRAVE
+#define KP_TID &kp TILDE
+#define KP_LBK &kp LBKT
+#define KP_RBK &kp RBKT
+#define KP_BSL &kp BSLH
+#define KP_APS &kp APOS
+#define KP_MIN &kp MINUS
+#define KP_DQT &kp DQT
+
+#define KP_PSC &kp PSCRN
+#define KP_SLK &kp SLCK
+#define KP_NLK &kp LNLCK
+#define KP_BRK &kp PAUSE_BREAK
+
+#define KP_INS &kp INSERT
+#define KP_DEL &kp DELETE
+#define KP_HOM &kp HOME
+#define KP_END &kp END
+
+#define KP_CTL &kp LCTRL
+#define KP_GUI &kp LGUI
+#define KP_ALT &kp LALT
+#define KP_SFT &kp LSHIFT
+
+#define KP_PUP &kp PAGE_UP
+#define KP_PDN &kp PAGE_DOWN
+#define KP_MIN &kp KP_MINUS
+#define KP_PLS &kp KP_PLUS
+#define KP_DOT &kp KP_DOT
+
+#define KP_UP_ &kp UP
+#define KP_RIT &kp RIGHT
+#define KP_LFT &kp LEFT
+#define KP_DN_ &kp DOWN
+
+
+// capsword
+#define CP_WRD &caps_word
+
+// one shot keys
+#define OS_SCP &kp LG(LS(S))
+
+#define MT_A__ &mt LSHIFT A
+#define MT_SMC &mt RSHIFT SEMICOLON
+#define MT_Z__ &mt LCTRL Z
+#define MT_X__ &mt LGUI X
+#define MT_C__ &mt LALT C
+#define MT_CMA &mt RALT COMMA
+#define MT_DOT &mt RGUI DOT
+#define MT_SLS &mt RCTL SLASH
+
+// num fn
+#define TD_1__ &TD1
+#define TD_2__ &TD2
+#define TD_3__ &TD3
+#define TD_4__ &TD4
+#define TD_5__ &TD5
+#define TD_6__ &TD6
+#define TD_7__ &TD7
+#define TD_8__ &TD8
+#define TD_9__ &TD9
+#define TD_0__ &TD10
+#define TD_SLS &TD11
+#define TD_AST &TD12
+
+// copy paste bracket
+#define TD_COP &TD_CCP
+#define TD_LPR &TD_LBKT
+#define TD_RPR &TD_RBKT
+
+// layers
+#define MO_SYM &mo SYM
+#define MO_NUM &mo NUM
+#define MO_CFG &mo CFG
+
+#define LT_ENT &lt NAV ENTER
+
+// bluetooth
+#define BT_CLR &bt BT_CLR
+#define BT_SE0 &bt BT_SEL 0
+#define BT_SE1 &bt BT_SEL 1
+#define BT_SE2 &bt BT_SEL 2
+#define BT_SE3 &bt BT_SEL 3
+#define BT_SE4 &bt BT_SEL 4
+
+// quantum
+#define __XX__ &none
+#define _TRNS_ &trans
